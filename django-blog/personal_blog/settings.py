d7b5c8b8007b705e0c096957bdf5c98f9495e24b
ljk28 <ljk28@hotmail.com>
2025-12-03 14:28:27 +0000

    Update settings.py

--- django-blog/personal_blog/settings.py
+++ django-blog/personal_blog/settings.py
@@ -61,6 +61,23 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # personal_blog/settings.py
+
+# ...
+
+INSTALLED_APPS = [
+    "blog.apps.BlogConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # personal_blog/settings.py
+
+]
+
+# ...
 ]
 
 # ...
